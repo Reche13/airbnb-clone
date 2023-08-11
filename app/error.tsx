2e80a6422f51c85241f14302913f27706a0369c2
Reche Soares <rechesoares13@gmail.com>
2023-08-11 14:57:48 +0530

    first commit

---
+++ app/error.tsx
@@ -0,0 +1,21 @@
+'use client'
+import React, { FC, useEffect } from 'react'
+import EmptyState from './components/EmptyState';
+ 
+interface ErrorStateProps {
+    error: Error;
+}
+
+const ErrorState:FC<ErrorStateProps> = ({error}) => {
+    useEffect(()=>{
+        console.log(error)
+    },[error])
+  return (
+    <EmptyState 
+        title='Uh Oh'
+        subtitle='Something went wrong'
+    />
+  )
+}
+
+export default ErrorState
